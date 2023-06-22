871da27844d0a476718bfd404358b125f03cc7db
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-22 15:12:20 -0300

    revised README.md and index.js for rendering with Apollo

--- book-search-engine/client/src/index.js
+++ book-search-engine/client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', // Replace with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
