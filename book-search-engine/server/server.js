eea120c83170163797d4ddc89bb4604ca92db59a
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-22 13:22:01 -0300

    update server.js and json

--- book-search-engine/server/server.js
+++ book-search-engine/server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
+const db = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
